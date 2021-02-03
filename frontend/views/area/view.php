99253faf4a3a969a5952b4dbbd1043a7d3bace22
mrstarmech <mrstarmech@users.noreply.github.com>
2021-02-03 18:26:20 +0700

    breadcrumbs shows item hierarchy (#7)

--- frontend/views/area/view.php
+++ frontend/views/area/view.php
@@ -1,7 +1,6 @@
 <?php
 
 /* @var $this yii\web\View */
-/* @var $archsite Archsite */
 /* @var $area Area */
 /* @var $petroglyphs Array */
 
@@ -12,9 +11,10 @@ use common\models\Area;
 use common\models\Petroglyph;
 
 $this->title = $area->name;
-
+$archsiteName = Archsite::find()->where(['id'=>$area->archsite_id])->one()->name;
 $this->params['breadcrumbs'] = [
-    ['label' => Yii::t('app', 'Site'), 'url' => ['archsite/'.$area->archsite_id]],
+    ['label' => Yii::t('app','Sites'), 'url' => '/archsite'],
+    ['label' => $archsiteName, 'url' => ['/archsite/'.$area->archsite_id]],
     $this->title,
 ];
 
--- frontend/views/petroglyph/view.php
+++ frontend/views/petroglyph/view.php
@@ -11,11 +11,30 @@ use common\models\Composition;
 use yii\helpers\Html;
 
 $this->title = $petroglyph->name;
-$this->params['breadcrumbs'] = [
-    ['label' => Yii::t('app', 'Petroglyphs'), 'url' => ['petroglyph/index']],
-    $this->title,
-];
+$archsite = \common\models\Archsite::find()->where(['id'=>$petroglyph->archsite_id])->one();
+$archsiteURL = $archsite == null ? null : '/archsite/'.$archsite->id;
+if($archsite != null){
+    $this->params['breadcrumbs'] = [
+        ['label' => Yii::t('app','Sites'), 'url' => '/archsite'],
+        ['label' => $archsite->name, 'url' => $archsiteURL],
+    ];
+}else{
+    $this->params['breadcrumbs'] = [
+        ['label' => Yii::t('app','Petroglyphs'), 'url' => '/petroglyph'],
+    ];
+}
 
+$area = $petroglyph->area_id == null ? null : \common\models\Area::find()->where(['id'=>$petroglyph->area_id])->one();
+$areaURL = $area == null ? '' : '/area/'.$area->id;
+
+if($area != null){
+    $this->params['breadcrumbs'][] = [
+        'label' => $area->name, 'url' => $areaURL,
+    ];
+}
+$this->params['breadcrumbs'][] = [
+    'label'=>$this->title,
+];
 $this->registerCssFile('css/petroglyph.css', ['depends' => ['yii\bootstrap\BootstrapPluginAsset']]);
 
 //$mdCol = Yii::$app->user->can('manager') ? 3 : 4;
