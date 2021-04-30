7cba127da6efbfcbb1956abc1711ccd28d2d14b0
mrstarmech <mr.starmech@gmail.comr>
2021-04-30 18:34:26 +0700

    rearrange petroglyph form

--- frontend/views/manager/_petroglyph_form.php
+++ frontend/views/manager/_petroglyph_form.php
@@ -24,112 +24,150 @@ use mihaildev\ckeditor\CKEditor;
 <div class="manager-_petroglyph_form">
 
     <?php $form = ActiveForm::begin(); ?>
-    <div class="row">
-        <div class="col-xs-12 col-md-6">
-            <?= $form->field($model, 'archsite_id')->dropDownList($archsites, ['prompt'=>Yii::t('manager', 'Select...'),
-                'onchange'=>'
-                    $.post("'.Url::toRoute('manager/area-list').'",
-                    {id : $(this).val()},
-                    function(data){
-                        $("select#petroglyph-area_id").html(data).attr("disabled",false);
-                    }
-                    );']) ?>
-            <?php if(!empty($areas)):?>
-            <?= $form->field($model, 'area_id')->dropDownList($areas, ['prompt'=>Yii::t('manager', 'Select...')]) ?>
-            <?php else:?>
-            <?= $form->field($model, 'area_id')->dropDownList($areas, ['prompt'=>Yii::t('manager', 'No areas on this site')]) ?>
-            <?php endif;?>
-            <?= $form->field($model, 'name') ?>
-            <?= $form->field($model, 'name_en') ?>
-            <?= $form->field($model, 'description')->widget(CKEditor::className(),
-                [
-                    'editorOptions' => [
-                        'preset' => 'standard',
-                        'inline' => false,
-                    ],
-                    'options' => [
-                        'allowedContent' => true,
-                    ],
+            <div class="row">
+                <div class="col-xs-6"><?= $form->field($model, 'archsite_id')->dropDownList($archsites, ['prompt'=>Yii::t('manager', 'Select...'),
+                    'onchange'=>'
+                        $.post("'.Url::toRoute('manager/area-list').'",
+                        {id : $(this).val()},
+                        function(data){
+                            $("select#petroglyph-area_id").html(data).attr("disabled",false);
+                        }
+                        );']) ?>
+                </div>
+                <div class="col-xs-3"><?= $form->field($model, 'public')->checkbox() ?></div>
+                <div class="col-xs-3">
+                    <?= Html::submitButton(Yii::t('manager', 'Save'), ['class' => 'btn btn-primary pull-right']) ?>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-6">
+                    <?php if(!empty($areas)):?>
+                    <?= $form->field($model, 'area_id')->dropDownList($areas, ['prompt'=>Yii::t('manager', 'Select...')]) ?>
+                    <?php else:?>
+                    <?= $form->field($model, 'area_id')->dropDownList($areas, ['prompt'=>Yii::t('manager', 'No areas on this site')]) ?>
+                    <?php endif;?>
+                </div>
+                <div class="col-xs-6"><?= $form->field($model, 'index') ?></div>
+            </div>
+            <div class="row">
+                <div class="col-xs-6"><?= $form->field($model, 'name') ?></div>
+                <div class="col-xs-6"><?= $form->field($model, 'name_en') ?></div>
+            </div>
+            <div class="row">
+                <div class="col-xs-6">
+                    <?= $form->field($model, 'description')->widget(CKEditor::className(),
+                        [
+                            'editorOptions' => [
+                                'preset' => 'standard',
+                                'inline' => false,
+                            ],
+                            'options' => [
+                                'allowedContent' => true,
+                            ],
 
-                ]) ?>
-            <?= $form->field($model, 'description_en')->widget(CKEditor::className(),
-                [
-                    'editorOptions' => [
-                        'preset' => 'standard',
-                        'inline' => false,
-                    ],
-                    'options' => [
-                        'allowedContent' => true,
-                    ],
+                        ]) ?>
+                </div>
+                <div class="col-xs-6">
+                    <?= $form->field($model, 'description_en')->widget(CKEditor::className(),
+                        [
+                            'editorOptions' => [
+                                'preset' => 'standard',
+                                'inline' => false,
+                            ],
+                            'options' => [
+                                'allowedContent' => true,
+                            ],
 
-                ]) ?>
-            <?= $form->field($model, 'lat') ?>
-            <?= $form->field($model, 'lng') ?>
-            <?= $form->field($model, 'culture_ids')->listBox($cultures, ['multiple' => true]) ?>
-            <?= $form->field($model, 'epoch_ids')->listBox($epochs, ['multiple' => true]) ?>
-            <?= $form->field($model, 'method_ids')->listBox($methods, ['multiple' => true]) ?>
-            <?= $form->field($model, 'style_ids')->listBox($styles, ['multiple' => true]) ?>
-            <?= $form->field($model, 'public')->checkbox() ?>
-            <?= $form->field($model, 'fileImage')->fileInput(), 
-                $form->field($model, 'fileDstr')->fileInput(),
-                $form->field($model, 'fileDraw')->fileInput(),
-                $form->field($model, 'fileReconstr')->fileInput(),
-                $form->field($model, 'fileOverlay')->fileInput() ?>
-            <?= $form->field($model, 'index') ?>
-            <?= $form->field($model, 'technical_description')->widget(CKEditor::className(),
-                [
-                    'editorOptions' => [
-                        'preset' => 'standard',
-                        'inline' => false,
-                    ],
-                    'options' => [
-                        'allowedContent' => true,
-                    ],
+                        ]) ?>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-6"><?= $form->field($model, 'lat') ?></div>
+                <div class="col-xs-6"><?= $form->field($model, 'lng') ?></div>
+            </div>
+            <div class="row">
+                <div class="col-xs-6"><?= $form->field($model, 'culture_ids')->listBox($cultures, ['multiple' => true]) ?></div>
+                <div class="col-xs-6"><?= $form->field($model, 'epoch_ids')->listBox($epochs, ['multiple' => true]) ?></div>
+            </div>
+            <div class="row">
+                <div class="col-xs-6"><?= $form->field($model, 'method_ids')->listBox($methods, ['multiple' => true]) ?></div>
+                <div class="col-xs-6"><?= $form->field($model, 'style_ids')->listBox($styles, ['multiple' => true]) ?></div>
+            </div>
+            <div class="row">
+                <div class="col-xs-6"><?= $form->field($model, 'fileImage')->fileInput()?></div>
+            </div>
+            <div class="row">
+                <div class="col-xs-6"><?=$form->field($model, 'fileDstr')->fileInput()?></div>
+            </div>
+            <div class="row">
+                <div class="col-xs-6"><?=$form->field($model, 'fileDraw')->fileInput()?></div>
+            </div>
+            <div class="row">
+                <div class="col-xs-6"><?=$form->field($model, 'fileReconstr')->fileInput()?></div>
+            </div>
+            <div class="row">
+                <div class="col-xs-6"><?=$form->field($model, 'fileOverlay')->fileInput()?></div>
+            </div>
+            <div class="row">
+                <div class="col-xs-6">
+                    <?= $form->field($model, 'technical_description')->widget(CKEditor::className(),
+                        [
+                            'editorOptions' => [
+                                'preset' => 'standard',
+                                'inline' => false,
+                            ],
+                            'options' => [
+                                'allowedContent' => true,
+                            ],
 
-                ]) ?>
-            <?= $form->field($model, 'technical_description_en')->widget(CKEditor::className(),
-                [
-                    'editorOptions' => [
-                        'preset' => 'standard',
-                        'inline' => false,
-                    ],
-                    'options' => [
-                        'allowedContent' => true,
-                    ],
+                        ]) ?>
+                </div>
+                <div class="col-xs-6">
+                    <?= $form->field($model, 'technical_description_en')->widget(CKEditor::className(),
+                        [
+                            'editorOptions' => [
+                                'preset' => 'standard',
+                                'inline' => false,
+                            ],
+                            'options' => [
+                                'allowedContent' => true,
+                            ],
 
-                ]) ?>
-            <?= $form->field($model, 'publication')->widget(CKEditor::className(),
-                [
-                    'editorOptions' => [
-                        'preset' => 'standard',
-                        'inline' => false,
-                    ],
-                    'options' => [
-                        'allowedContent' => true,
-                    ],
+                        ]) ?>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-6">
+                    <?= $form->field($model, 'publication')->widget(CKEditor::className(),
+                        [
+                            'editorOptions' => [
+                                'preset' => 'standard',
+                                'inline' => false,
+                            ],
+                            'options' => [
+                                'allowedContent' => true,
+                            ],
 
-                ]) ?>
-            <?= $form->field($model, 'publication_en')->widget(CKEditor::className(),
-                [
-                    'editorOptions' => [
-                        'preset' => 'standard',
-                        'inline' => false,
-                    ],
-                    'options' => [
-                        'allowedContent' => true,
-                    ],
+                        ]) ?>
+                </div>
+                <div class="col-xs-6">
+                    <?= $form->field($model, 'publication_en')->widget(CKEditor::className(),
+                        [
+                            'editorOptions' => [
+                                'preset' => 'standard',
+                                'inline' => false,
+                            ],
+                            'options' => [
+                                'allowedContent' => true,
+                            ],
 
-                ]) ?>
-            <?= $form->field($model, 'author_page') ?>
-            <?= $form->field($model, 'author_page_en') ?>
-        </div>
-        <div class="col-xs-12 col-md-6 text-right">
-            <div class="form-group">
-                <?= Html::submitButton(Yii::t('manager', 'Save'), ['class' => 'btn btn-primary']) ?>
+                        ]) ?>
+                </div>
+            </div>
+            <div class="row">
+                    <div class="col-xs-6"><?= $form->field($model, 'author_page') ?></div>
+                    <div class="col-xs-6"><?= $form->field($model, 'author_page_en') ?></div>
             </div>
         </div>
-    </div>
-
     <?php ActiveForm::end(); ?>
-
-</div><!-- manager-_petroglyph_form -->
+<!-- manager-_petroglyph_form -->
