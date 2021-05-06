0a6022f80e6ba37e18bac6e6e551baf961a6ef96
mrstarmech <mr.starmech@gmail.comr>
2021-05-06 18:44:32 +0700

    improve & image authors

--- frontend/controllers/PetroglyphController.php
+++ frontend/controllers/PetroglyphController.php
@@ -108,31 +108,76 @@ class PetroglyphController extends BaseController
         if(!empty($petroglyph->image)){
             $petroglyph_image_objects[] = [
                 'name' => Yii::t('model', 'Image'),
-                'image' => Petroglyph::SRC_IMAGE . '/' . $petroglyph->thumbnailImage
+                'image' => Petroglyph::SRC_IMAGE . '/' . $petroglyph->thumbnailImage,
+                'author' => $petroglyph->img_author,
+                'copyright' => $petroglyph->img_copyright,
+                'source' => $petroglyph->img_source
             ];
         }
         if(!empty($petroglyph->im_dstretch)){
             $petroglyph_image_objects[] = [
                 'name' => Yii::t('model', 'Image DStretch'),
-                'image' => Petroglyph::SRC_IMAGE . '/' . $petroglyph->thumbnailImDstretch
+                'image' => Petroglyph::SRC_IMAGE . '/' . $petroglyph->thumbnailImDstretch,
+                'author' => $petroglyph->ds_img_author,
+                'copyright' => $petroglyph->ds_img_copyright,
+                'source' => $petroglyph->ds_img_source
             ];
         }
         if(!empty($petroglyph->im_drawing)){
             $petroglyph_image_objects[] = [
                 'name' => Yii::t('model', 'Drawing image'),
-                'image' => Petroglyph::SRC_IMAGE . '/' . $petroglyph->thumbnailImDrawing
+                'image' => Petroglyph::SRC_IMAGE . '/' . $petroglyph->thumbnailImDrawing,
+                'author' => $petroglyph->dr_img_author,
+                'copyright' => $petroglyph->dr_img_copyright,
+                'source' => $petroglyph->dr_img_source
             ];
         }
         if(!empty($petroglyph->im_overlay)){
             $petroglyph_image_objects[] = [
                 'name' => Yii::t('model', 'Image overlay'),
-                'image' => Petroglyph::SRC_IMAGE . '/' . $petroglyph->thumbnailImOverlay
+                'image' => Petroglyph::SRC_IMAGE . '/' . $petroglyph->thumbnailImOverlay,
+                'author' => $petroglyph->ov_img_author,
+                'copyright' => $petroglyph->ov_img_copyright,
+                'source' => $petroglyph->ov_img_source
             ];
         }
         if(!empty($petroglyph->im_reconstruction)){
             $petroglyph_image_objects[] = [
                 'name' => Yii::t('model', 'Reconstruction image'),
-                'image' => Petroglyph::SRC_IMAGE . '/' . $petroglyph->thumbnailImReconstr
+                'image' => Petroglyph::SRC_IMAGE . '/' . $petroglyph->thumbnailImReconstr,
+                'author' => $petroglyph->re_img_author,
+                'copyright' => $petroglyph->re_img_copyright,
+                'source' => $petroglyph->re_img_source
+            ];
+        }
+
+        $petroglyph_attrib_objects = [];
+        if(!empty($petroglyph->cultures)){
+            $petroglyph_attrib_objects[] = [
+                'name' => Yii::t('app', 'Culture'),
+                'data' => implode (", ",
+                    array_map(function($obj) { return $obj->name; }, $petroglyph->cultures))
+            ];
+        }
+        if(!empty($petroglyph->epochs)){
+            $petroglyph_attrib_objects[] = [
+                'name' => Yii::t('app', 'Epoch'),
+                'data' => implode (", ",
+                    array_map(function($obj) { return $obj->name; }, $petroglyph->epochs))
+            ];
+        }
+        if(!empty($petroglyph->styles)){
+            $petroglyph_attrib_objects[] = [
+                'name' => Yii::t('app', 'Style'),
+                'data' => implode (", ",
+                    array_map(function($obj) { return $obj->name; }, $petroglyph->styles))
+            ];
+        }
+        if(!empty($petroglyph->methods)){
+            $petroglyph_attrib_objects[] = [
+                'name' => Yii::t('app', 'Method'),
+                'data' => implode (", ",
+                    array_map(function($obj) { return $obj->name; }, $petroglyph->methods))
             ];
         }
 
@@ -149,18 +194,23 @@ class PetroglyphController extends BaseController
             <hr>
             <table width="100%">
                 <tr>
-                    <td width="40%">' . Yii::t('app', 'Lab "LIA ARTEMIR"') . '</td>
-                    <td width="20%" align="center">{PAGENO}/{nbpg}</td>
-                    <td width="40%" style="text-align: right;">' . Yii::t('app', 'Novosibirsk State University') . '</td>
+                    <td width="45%">' . Yii::t('app', 'Lab "LIA ARTEMIR"') . '</td>
+                    <td width="10%" align="center">{PAGENO}</td>
+                    <td width="45%" style="text-align: right;">' . Yii::t('app', 'Novosibirsk State University') . '</td>
                 </tr>
                 <tr>
-                    <td width="40%">' . HTML::a('www.rockart.artemiris.org', 'http://rockart.artemiris.org/') . '</td>
-                    <td width="20%" align="center"></td>
-                    <td width="40%" style="text-align: right;">' . Yii::t('app', 'Project supported by RNF #18-78-10079') . '</td>
+                    <td width="45%">' . HTML::a('rockart.artemiris.org/petroglyph/' . $petroglyph->id,
+                                                'http://rockart.artemiris.org/' . Yii::$app->language . '/petroglyph/' . $petroglyph->id) . '</td>
+                    <td width="10%" align="center"></td>
+                    <td width="45%" style="text-align: right;">' . Yii::t('app', 'Project supported by RNF #18-78-10079') . '</td>
                 </tr>
             </table>'
         );
-        $mpdf->WriteHTML($this->renderPartial('pdf_view',['petroglyph'=>$petroglyph, 'image_objects' => $petroglyph_image_objects]));
-        $mpdf->Output('test.pdf', 'D');
+        $mpdf->WriteHTML($this->renderPartial('pdf_view', [
+            'petroglyph'=>$petroglyph,
+            'image_objects' => $petroglyph_image_objects,
+            'attrib_objects' => $petroglyph_attrib_objects
+        ]));
+        $mpdf->Output($petroglyph->name . '.pdf', 'D');
     }
 }
--- frontend/views/petroglyph/pdf_view.php
+++ frontend/views/petroglyph/pdf_view.php
@@ -4,8 +4,8 @@ use yii\helpers\Html;
 
 /* @var $petroglyph Petroglyph */
 /* @var $image_objects Array */
+/* @var $attrib_objects Array */
 
-$line_cnt = intdiv((count($image_objects) + 1), 2);
 ?>
 
 <style>
@@ -14,6 +14,9 @@ $line_cnt = intdiv((count($image_objects) + 1), 2);
     }
     .td_cult{
         width: 50%;
+    }
+    .td_a{
+        font-size: 8pt;
     }
     .tb_img{
         width: 100%;
@@ -40,108 +43,125 @@ $line_cnt = intdiv((count($image_objects) + 1), 2);
     <h3><?= Yii::t('app', 'Technical description') ?></h3>
     <?= $petroglyph->technical_description ?>
 <?php endif; ?>
-
-
+<?php $line_cnt = intdiv((count($image_objects) + 1), 2); ?>
+<?php if($line_cnt > 0): ?>
+<table>
+    <tr>
+        <td>
+            <?php for ($i = 0; $i < $line_cnt; $i++):?>
+                <table class="tb_img">
+                    <tr>
+                        <?php $image_object = array_shift($image_objects)?>
+                        <td class="td_img_block">
+                            <table autosize="1">
+                                <tr>
+                                    <td>
+                                        <span style="font-size: 12pt; font-weight: bold"><?= $image_object ? $image_object['name'] : '' ?></span>
+                                    </td>
+                                </tr>
+                            </table>
+                            <table autosize="1">
+                                <tr>
+                                    <td>
+                                        <?= $image_object ? Html::img($image_object['image'],['class'=>'t_img']) : '' ?>
+                                    </td>
+                                </tr>
+                            </table>
+                            <table autosize="1">
+                                <tr>
+                                    <td class="td_a">
+                                        <?= $image_object && $image_object['author'] ? Yii::t('model','Image authors') . ': ' . $image_object['author'] . '<br>' : '' ?>
+                                        <?= $image_object && $image_object['copyright'] ? Yii::t('model','Image copyright') . ': ' . $image_object['copyright'] . '<br>' : '' ?>
+                                        <?= $image_object && $image_object['source'] ? Yii::t('model','Image source') . ': ' . $image_object['source'] : '' ?>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                        <?php $image_object = array_shift($image_objects)?>
+                        <td class="td_img_block">
+                            <table autosize="1">
+                                <tr>
+                                    <td>
+                                        <span style="font-size: 12pt; font-weight: bold"><?= $image_object ? $image_object['name'] : '' ?></span>
+                                    </td>
+                                </tr>
+                            </table>
+                            <table autosize="1">
+                                <tr>
+                                    <td>
+                                        <?= $image_object ? Html::img($image_object['image'],['class'=>'t_img']) : '' ?>
+                                    </td>
+                                </tr>
+                            </table>
+                            <table autosize="1">
+                                <tr>
+                                    <td class="td_a">
+                                        <?= $image_object && $image_object['author'] ? Yii::t('model','Image authors') . ': ' . $image_object['author'] . '<br>' : '' ?>
+                                        <?= $image_object && $image_object['copyright'] ? Yii::t('model','Image copyright') . ': ' . $image_object['copyright'] . '<br>' : '' ?>
+                                        <?= $image_object && $image_object['source'] ? Yii::t('model','Image source') . ': ' . $image_object['source'] : '' ?>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                </table>
+            <?php endfor;?>
+        </td>
+    </tr>
+</table>
+<?php endif; ?>
+<?php $line_cnt = intdiv((count($attrib_objects) + 1), 2); ?>
+<?php if($line_cnt > 0): ?>
 <br>
 <table>
     <tr>
         <td>
-<?php for ($i = 0; $i < $line_cnt; $i++):?>
-    <table class="tb_img">
-        <tr>
-            <?php $image_object = array_shift($image_objects)?>
-            <td class="td_img_block">
-                <table autosize="1">
+            <?php for ($i = 0; $i < $line_cnt; $i++):?>
+                <table class="tb_img">
                     <tr>
-                        <td>
-                            <span style="font-size: 12pt; font-weight: bold"><?= $image_object ? $image_object['name'] : '' ?></span>
+                        <?php $attrib_object = array_shift($attrib_objects)?>
+                        <td class="td_img_block">
+                            <table autosize="1">
+                                <tr>
+                                    <td>
+                                        <span style="font-size: 12pt; font-weight: bold"><?= $attrib_object ? $attrib_object['name'] : '' ?></span>
+                                    </td>
+                                </tr>
+                            </table>
+                            <table autosize="1">
+                                <tr>
+                                    <td>
+                                        <?= $attrib_object ? $attrib_object['data'] : '' ?>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                        <?php $attrib_object = array_shift($attrib_objects)?>
+                        <td class="td_img_block">
+                            <table autosize="1">
+                                <tr>
+                                    <td>
+                                        <span style="font-size: 12pt; font-weight: bold"><?= $attrib_object ? $attrib_object['name'] : '' ?></span>
+                                    </td>
+                                </tr>
+                            </table>
+                            <table autosize="1">
+                                <tr>
+                                    <td>
+                                        <?= $attrib_object ? $attrib_object['data'] : '' ?>
+                                    </td>
+                                </tr>
+                            </table>
                         </td>
                     </tr>
                 </table>
-                <table autosize="1">
-                    <tr>
-                        <td>
-                            <?= $image_object ? Html::img($image_object['image'],['class'=>'t_img']) : '' ?>
-                        </td>
-                    </tr>
-                </table>
-            </td>
-            <?php $image_object = array_shift($image_objects)?>
-            <td class="td_img_block">
-                <table autosize="1">
-                    <tr>
-                        <td>
-                            <span style="font-size: 12pt; font-weight: bold"><?= $image_object ? $image_object['name'] : '' ?></span>
-                        </td>
-                    </tr>
-                </table>
-                <table autosize="1">
-                    <tr>
-                        <td>
-                            <?= $image_object ? Html::img($image_object['image'],['class'=>'t_img']) : '' ?>
-                        </td>
-                    </tr>
-                </table>
-            </td>
-        </tr>
-    </table>
-<?php endfor;?>
+            <?php endfor;?>
         </td>
     </tr>
 </table>
-<br>
-<table style="page-break-inside: avoid; width: 100%" autosize="1">
-    <tr>
-        <td class="td_cult"><h4><?= Yii::t('app', 'Culture')?></h4></td>
-        <td class="td_cult"><h4><?= Yii::t('app', 'Epoch')?></h4></td>
-    </tr>
-    <tr>
-        <?php $data = [(isset($petroglyph->cultures) ? implode (", ",
-            array_map(function($obj) { return $obj->name; }, $petroglyph->cultures)) : null)] ?>
-        <?php if (!empty($data)): ?>
-            <td class="td_cult">
-                <?php foreach ($data as $datum): ?>
-                        <?= $datum ?><br>
-                <?php endforeach; ?>
-            </td>
-        <?php endif; ?>
-        <?php $data = [(isset($petroglyph->epochs) ? implode (", ",
-            array_map(function($obj) { return $obj->name; }, $petroglyph->epochs)) : null)] ?>
-        <?php if (!empty($data)): ?>
-            <td class="td_cult">
-                <?php foreach ($data as $datum): ?>
-                    <?= $datum ?><br>
-                <?php endforeach; ?>
-            </td>
-        <?php endif; ?>
-    </tr>
-    <tr>
-        <td class="td_cult"><h4><?= Yii::t('app', 'Method')?></h4></td>
-        <td class="td_cult"><h4><?= Yii::t('app', 'Style')?></h4></td>
-    </tr>
-    <tr>
-        <?php $data = [(isset($petroglyph->methods) ? implode (", ",
-            array_map(function($obj) { return $obj->name; }, $petroglyph->methods)) : null)] ?>
-        <?php if (!empty($data)): ?>
-            <td class="td_cult">
-                <?php foreach ($data as $datum): ?>
-                    <?= $datum ?><br>
-                <?php endforeach; ?>
-            </td>
-        <?php endif; ?>
-        <?php $data = [(isset($petroglyph->styles) ? implode (", ",
-            array_map(function($obj) { return $obj->name; }, $petroglyph->styles)) : null)] ?>
-        <?php if (!empty($data)): ?>
-            <td class="td_cult">
-                <?php foreach ($data as $datum): ?>
-                    <?= $datum ?><br>
-                <?php endforeach; ?>
-            </td>
-        <?php endif; ?>
-    </tr>
-</table>
-<br>
+<?php endif;?>
 <?php if (!empty($petroglyph->publication)): ?>
+<br>
     <table  style="page-break-inside: avoid" autosize="1">
         <tr>
             <td>
