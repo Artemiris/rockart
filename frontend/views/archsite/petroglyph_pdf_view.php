d4c83b3da36c3cf48526db4990d19f0e4efab2a4
mrstarmech <mrstarmech@users.noreply.github.com>
2021-05-13 14:42:02 +0700

    Update PDF Layout (#23)

--- frontend/views/archsite/petroglyph_pdf_view.php
+++ frontend/views/archsite/petroglyph_pdf_view.php
@@ -9,43 +9,23 @@ use yii\helpers\Html;
 
 ?>
 
-    <style>
-        .t_img{
-            width: 46%;
-        }
-        .td_cult{
-            width: 50%;
-        }
-        span.head{
-            font-size: 14pt;
-            font-weight: bold;
-        }
-        span.body{
-            font-size: 14pt;
-        }
-        .tb_out{
-            padding-left: -1mm;
-        }
-        .td_a{
-            font-size: 8pt;
-        }
-        .tb_img{
-            width: 100%;
-            vertical-align: top;
-        }
-        .tb_att{
-            width: 100%;
-            page-break-inside: avoid;
-            vertical-align: top;
-        }
-        .td_img_block{
-            width: 50%;
-            vertical-align: top;
-        }
-    </style>
-    <h1>
-        <?= empty($parentName) ? $petroglyph->name : $parentName . $petroglyph->name ?>
-    </h1>
+<style>
+    .attr_t_w200{
+        width: 200mm;
+        page-break-inside: avoid;
+        table-layout: fixed;
+    }
+    .attr_t_w100{
+        width: 100mm;
+        padding-left: -0.5mm;
+    }
+    .image_05{
+        max-width: 99mm;
+    }
+</style>
+<h1>
+    <?= empty($parentName) ? $petroglyph->name : $parentName . $petroglyph->name ?>
+</h1>
 <?php if(!empty($image_objects)):?>
     <?php $image_main = array_shift($image_objects) ?>
     <?= Html::img($image_main['image']) ?>
@@ -63,113 +43,84 @@ use yii\helpers\Html;
 <?php endif; ?>
 <?php $line_cnt = intdiv((count($attrib_objects) + 1), 2); ?>
 <?php if($line_cnt > 0): ?>
-    <br>
-    <table class="tb_att" style="width: 210mm" autosize="1">
-        <tr>
-            <td>
-                <?php for ($i = 0; $i < $line_cnt; $i++):?>
-                    <table class="tb_img" style="width: 210mm" autosize="1">
+    <table class="attr_t_w200" autosize="1">
+        <?php for ($i = 0; $i < $line_cnt; $i++):?>
+            <tr>
+                <td class="attr_t_w100">
+                    <?php $attrib_object = array_shift($attrib_objects)?>
+                    <table>
                         <tr>
-                            <?php $attrib_object = array_shift($attrib_objects)?>
-                            <td class="td_img_block tb_out">
-                                <table autosize="1">
-                                    <tr>
-                                        <td class="tb_out">
-                                            <span class="head"><?= $attrib_object ? $attrib_object['name'] : '' ?></span>
-                                        </td>
-                                    </tr>
-                                </table>
-                                <table autosize="1">
-                                    <tr>
-                                        <td class="tb_out">
-                                            <span class="body"><?= $attrib_object ? $attrib_object['data'] : '' ?></span>
-                                        </td>
-                                    </tr>
-                                </table>
+                            <td class="attr_t_w100">
+                                <h4><?= $attrib_object ? $attrib_object['name'] : '' ?></h4>
                             </td>
-                            <?php $attrib_object = array_shift($attrib_objects)?>
-                            <td class="td_img_block">
-                                <table autosize="1">
-                                    <tr>
-                                        <td>
-                                            <span class="head"><?= $attrib_object ? $attrib_object['name'] : '' ?></span>
-                                        </td>
-                                    </tr>
-                                </table>
-                                <table autosize="1">
-                                    <tr>
-                                        <td>
-                                            <span class="body"><?= $attrib_object ? $attrib_object['data'] : '' ?></span>
-                                        </td>
-                                    </tr>
-                                </table>
+                        </tr>
+                        <tr>
+                            <td class="attr_t_w100">
+                                <?= $attrib_object ? $attrib_object['data'] : '' ?>
                             </td>
                         </tr>
                     </table>
-                <?php endfor;?>
-            </td>
-        </tr>
+                </td>
+                <td class="attr_t_w100">
+                    <?php $attrib_object = array_shift($attrib_objects)?>
+                    <table>
+                        <tr>
+                            <td class="attr_t_w100">
+                                <h4><?= $attrib_object ? $attrib_object['name'] : '' ?></h4>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="attr_t_w100">
+                                <?= $attrib_object ? $attrib_object['data'] : '' ?>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        <?php endfor;?>
     </table>
 <?php endif;?>
 <?php $line_cnt = intdiv((count($image_objects) + 1), 2); ?>
 <?php if($line_cnt > 0): ?>
-    <table style="width: 210mm; table-layout: fixed">
+    <table class="attr_t_w200" autosize="1">
         <tr>
-            <td>
+            <td class="attr_t_w200">
                 <?php for ($i = 0; $i < $line_cnt; $i++):?>
-                    <table class="tb_img">
+                    <table>
                         <tr>
-                            <?php $image_object = array_shift($image_objects)?>
-                            <td class="td_img_block tb_out">
-                                <table autosize="1">
-                                    <tr>
-                                        <td class="tb_out">
-                                            <span class="head"><?= $image_object ? $image_object['name'] : '' ?></span>
-                                        </td>
-                                    </tr>
-                                </table>
-                                <table autosize="1">
-                                    <tr>
-                                        <td class="tb_out">
-                                            <?= $image_object ? Html::img($image_object['image'],['class'=>'t_img']) : '' ?>
-                                        </td>
-                                    </tr>
-                                </table>
-                                <table autosize="1">
-                                    <tr>
-                                        <td class="td_a tb_out">
-                                            <?= $image_object && $image_object['author'] ? Yii::t('model','Image authors') . ': ' . $image_object['author'] . '<br>' : '' ?>
-                                            <?= $image_object && $image_object['copyright'] ? Yii::t('model','Image copyright') . ': ' . $image_object['copyright'] . '<br>' : '' ?>
-                                            <?= $image_object && $image_object['source'] ? Yii::t('model','Image source') . ': ' . $image_object['source'] : '' ?>
-                                        </td>
-                                    </tr>
-                                </table>
+                            <td class="attr_t_w100">
+                                <?php $image_object = array_shift($image_objects)?>
+                                <?php if(!empty($image_object)):?>
+                                    <table>
+                                        <tr>
+                                            <td class="attr_t_w100">
+                                                <h4><?= $image_object['name'] ?></h4>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td class="attr_t_w100">
+                                                <?= Html::img($image_object['image'],['class'=>'image_05']) ?>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                <?php endif; ?>
                             </td>
-                            <?php $image_object = array_shift($image_objects)?>
-                            <td class="td_img_block">
-                                <table autosize="1">
-                                    <tr>
-                                        <td>
-                                            <span class="head"><?= $image_object ? $image_object['name'] : '' ?></span>
-                                        </td>
-                                    </tr>
-                                </table>
-                                <table autosize="1">
-                                    <tr>
-                                        <td>
-                                            <?= $image_object ? Html::img($image_object['image'],['class'=>'t_img']) : '' ?>
-                                        </td>
-                                    </tr>
-                                </table>
-                                <table autosize="1">
-                                    <tr>
-                                        <td class="td_a">
-                                            <?= $image_object && $image_object['author'] ? Yii::t('model','Image authors') . ': ' . $image_object['author'] . '<br>' : '' ?>
-                                            <?= $image_object && $image_object['copyright'] ? Yii::t('model','Image copyright') . ': ' . $image_object['copyright'] . '<br>' : '' ?>
-                                            <?= $image_object && $image_object['source'] ? Yii::t('model','Image source') . ': ' . $image_object['source'] : '' ?>
-                                        </td>
-                                    </tr>
-                                </table>
+                            <td class="attr_t_w100">
+                                <?php $image_object = array_shift($image_objects)?>
+                                <?php if(!empty($image_object)):?>
+                                    <table>
+                                        <tr>
+                                            <td class="attr_t_w100">
+                                                <h4><?= $image_object['name'] ?></h4>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td class="attr_t_w100">
+                                                <?= Html::img($image_object['image'],['class'=>'image_05']) ?>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                <?php endif; ?>
                             </td>
                         </tr>
                     </table>
@@ -180,15 +131,13 @@ use yii\helpers\Html;
 <?php endif; ?>
 <?php if (!empty($petroglyph->publication)): ?>
     <br>
-    <table  style="page-break-inside: avoid" autosize="1">
-        <tr>
-            <td>
-                <h3><?= Yii::t('app', 'Publications') ?></h3>
-            </td>
-        </tr>
-        <tr><td></td></tr>
-        <tr>
-            <td><?= $petroglyph->publication ?></td>
-        </tr>
-    </table>
+    <div style="page-break-inside: avoid">
+        <h3><?= Yii::t('app', 'Publications') ?></h3>
+        <?= $petroglyph->publication ?>
+    </div>
+<?php endif; ?>
+<?php if(!empty($petroglyph->author_page)):?>
+    <div>
+        <span style="font-size: 14px; font-style: italic"><?= Yii::t('app', 'Description authors') . ': ' . $petroglyph->author_page ?></span>
+    </div>
 <?php endif; ?>
